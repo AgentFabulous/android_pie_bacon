f8c9c27abc841d47e09cb3350e89e07b1709a021
Ethan Chen <intervigil@gmail.com>
2016-06-22 12:47:55 -0700

    hal: Don't make missing CSD init/deinit functions fatal
    * Not all CSD libraries provide these functions
    
    Change-Id: I991601898a8b5ecea0e893f8412bfd014f4eee44

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -1010,9 +1010,8 @@ static struct csd_data *open_csd_client(bool i2s_ext_modem)
         csd->deinit = (deinit_t)dlsym(csd->csd_client,
                                              "csd_client_deinit");
         if (csd->deinit == NULL) {
-            ALOGE("%s: dlsym error %s for csd_client_deinit", __func__,
+            ALOGW("%s: dlsym error %s for csd_client_deinit", __func__,
                   dlerror());
-            goto error;
         }
         csd->disable_device = (disable_device_t)dlsym(csd->csd_client,
                                              "csd_client_disable_device");
@@ -1111,9 +1110,8 @@ static struct csd_data *open_csd_client(bool i2s_ext_modem)
         csd->init = (init_t)dlsym(csd->csd_client, "csd_client_init");
 
         if (csd->init == NULL) {
-            ALOGE("%s: dlsym error %s for csd_client_init",
+            ALOGW("%s: dlsym error %s for csd_client_init",
                   __func__, dlerror());
-            goto error;
         } else {
             csd->init(i2s_ext_modem);
         }
@@ -1129,7 +1127,11 @@ error:
 void close_csd_client(struct csd_data *csd)
 {
     if (csd != NULL) {
-        csd->deinit();
+        if (csd->deinit == NULL) {
+            ALOGW("%s: CSD deinit function not present", __func__);
+        } else {
+            csd->deinit();
+        }
         dlclose(csd->csd_client);
         free(csd);
         csd = NULL;

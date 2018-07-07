Fabulous-Oreo/build_make

envsetup: Fix lunch menu for ZSH
Change-Id: I7906b32dc5a3c3aee0f8871811a28cb50425ef73
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

envsetup.sh
@@ -583,7 +583,11 @@ function lunch()
     then
         if [ $answer -le ${#LUNCH_MENU_CHOICES[@]} ]
         then
-            selection=${LUNCH_MENU_CHOICES[$(($answer-1))]}
+            if [ "$(__detect_shell)" == "zsh" ]; then
+                selection=${LUNCH_MENU_CHOICES[$(($answer))]}
+            else
+                selection=${LUNCH_MENU_CHOICES[$(($answer-1))]}
+            fi
         fi
     else
         selection=$answer

AgentFabulous/android_pie_bacon

envsetup.sh
@@ -579,7 +579,11 @@ function lunch()
         local choices=($(TARGET_BUILD_APPS= LUNCH_MENU_CHOICES="${LUNCH_MENU_CHOICES[@]}" get_build_var COMMON_LUNCH_CHOICES))
         if [ $answer -le ${#choices[@]} ]
         then
-            selection=${choices[$(($answer-1))]}
+            if [ "$(__detect_shell)" == "zsh" ]; then
+                selection=${choices[$(($answer))]}
+            else
+                selection=${choices[$(($answer-1))]}
+            fi
         fi
     else
         selection=$answer
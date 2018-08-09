AgentFabulous/android_oreo_bacon

Revert "msm8974: remove from top level makefile"
This reverts commit 9e8b76d32ece15e79ebf4b02ede869d89807eec6.

Change-Id: I67e9cee2cc06fb56f8ca7363bcdc51658d702876

Android.mk
@@ -9,7 +9,7 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     QCOM_MEDIA_ROOT := $(call my-dir)/msm8998
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
     include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
   endif
@@ -18,11 +18,11 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     include $(QCOM_MEDIA_ROOT)/mm-video-legacy/Android.mk
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8610 msm8226 msm8974 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8960 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
   endif
 endif

AgentFabulous/android_pie_bacon

Android.mk
@@ -12,7 +12,7 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     QCOM_MEDIA_ROOT := $(call my-dir)/sdm845
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8974 msm8610 msm8226 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
     include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
   endif
@@ -21,11 +21,11 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     include $(QCOM_MEDIA_ROOT)/mm-video-legacy/Android.mk
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8974 msm8610 msm8226 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8960 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8974 msm8610 msm8226 msm8960 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
   endif
 endif
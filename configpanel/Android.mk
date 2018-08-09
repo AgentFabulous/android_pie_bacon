0ecb3865c8d55d3d91c0ae3ab964d837b9eaa407
Kshitij Gupta <kshitijgm@gmail.com>
2018-08-09 21:15:51 +0530

    configpanel: set LOCAL_SDK_VERSION
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -20,6 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SDK_VERSION := current
 LOCAL_PACKAGE_NAME := ConfigPanel
 LOCAL_MODULE_TAGS := optional
 
